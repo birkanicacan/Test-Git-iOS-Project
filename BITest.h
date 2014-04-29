6f9464559d99bed7c42475d1c52e13994bf8a87a
Birkan Icacan <birkanicacan@gmail.com>
2014-04-28 20:51:45 -0700

    added a test class

---
+++ BITest.h
@@ -0,0 +1,13 @@
+//
+//  BITest.h
+//  Git Test
+//
+//  Created by Birkan Icacan on 4/28/14.
+//  Copyright (c) 2014 Icacan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BITest : NSObject
+
+@end
